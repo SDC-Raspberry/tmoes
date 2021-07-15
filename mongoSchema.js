87a849893cb74896e4b6c3770c128d64bf08c016
Tim Moses <trmoses42@gmail.com>
2021-07-14 20:52:54 -0500

    Refactored schema

--- mongoSchema.js
+++ mongoSchema.js
@@ -1,43 +1,52 @@
 var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/atelier', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
 var Schema = mongoose.Schema;
 
+db.on('error', console.error.bind(console, 'Error connecting to database'));
+
+db.once('open', function() {
 // ------------- QUESTIONS
-var QuestionsSchema = new Schema({
-  question_id: Number,
-  question_body: String,
-  question_date: Date,
-  asker_name: String,
-  question_helpfulness: Number,
-  reported: Number
+  var QuestionsSchema = new Schema({
+    id: Number,
+    product_id: Number,
+    body: String,
+    date_written: Date,
+    asker_name: String,
+    asker_email: String,
+    reported: Number,
+    helpful: Number
+  });
+
+  var QuestionsModel = mongoose.model('QuestionsModel', QuestionsSchema);
+
+  // ------------- ANSWERS
+  var AnswersSchema = new Schema({
+    id: Number,
+    question_id: Number,
+    body: String,
+    date_written: Date,
+    answerer_name: String,
+    answerer_email: String,
+    reported: Number,
+    helpful: Number,
+  });
+
+  var AnswersModel = mongoose.model('AnswersModel', AnswersSchema);
+
+  // ------------- PHOTOS
+  var AnswersPhotosSchema = new Schema({
+    id: Number,
+    answer_id: Number,
+    url: String
+  });
+
+  var AnswersPhotosModel = mongoose.model('AnswersPhotosModel', AnswersPhotosSchema);
 });
 
-var QuestionsModel = mongoose.model('QuestionsModel', QuestionsSchema);
-
-// ------------- ANSWERS
-var AnswersSchema = new Schema({
-  answer_id: Number,
-  question_id: Number,
-  answer_body: String,
-  answer_date: Date,
-  answer_name: String,
-  helpfulness: Number,
-  photos:
-  [
-    {
-      photo_id: Number,
-      photo_url: String
-    }
-  ]
-});
-
-var AnswersModel = mongoose.model('AnswersModel', AnswersSchema);
-
-// ------------- PHOTOS
-var PhotoSchema = new Schema({
-  answer_id: Number,
-  photo_id: Number,
-  photo_url: String
-});
-
-var PhotoModel = mongoose.model('PhotoModel', PhotoSchema);
